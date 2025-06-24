768f214e5a289fbcac533255c0cf25b19745921f
BryanJC <106637053+BryanJC-DEV@users.noreply.github.com>
2025-06-23 18:46:38 -0600

    Poniéndome al día.
    Primer commit. Lo siento, profe, no tengo PC y lo estoy haciendo en una computadora prestada hasta ahora.

---
+++ MNProyecto-main/MNProyecto-main/Controllers/homeController.php
@@ -0,0 +1,58 @@
+<?php
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/Curso/Models/homeModel.php';
+
+if(isset($_POST["btnIniciarSesion"]))
+{
+    $correo = $_POST["txtCorreo"];
+    $contrasenna = $_POST["txtContrasenna"];
+
+    $respuesta = ValidarInicioSesionModel($correo, $contrasenna);
+
+    if($respuesta != null && $respuesta -> num_rows > 0)
+    {
+        header("location: ../../Views/Home/principal.php");
+    }
+    else
+    {
+        $_POST["txtMensaje"] = "Su información no fue validada correctamente.";
+    }
+}
+
+if(isset($_POST["btnRegistrarUsuario"]))
+{
+    $nombre = $_POST["txtNombre"];
+    $correo = $_POST["txtCorreo"];
+    $identificacion = $_POST["txtIdentificacion"];
+    $contrasenna = $_POST["txtContrasenna"];
+
+    $respuesta = RegistrarUsuarioModel($nombre, $correo, $identificacion, $contrasenna);
+
+    if($respuesta)
+    {
+        header("location: ../../Views/Home/login.php");
+    }
+    else
+    {
+        $_POST["txtMensaje"] = "Su información no fue registrada correctamente.";
+    }
+}
+
+if(isset($_POST["btnRecuperarAcceso"]))
+{
+    $correo = $_POST["txtCorreo"];
+
+    $respuesta = ValidarCorreoModel($correo);
+
+    if($respuesta != null && $respuesta -> num_rows > 0)
+    {
+        //Tomar los datos y enviar el correo electrónico al usuario
+
+        header("location: ../../Views/Home/login.php");
+    }
+    else
+    {
+        $_POST["txtMensaje"] = "Su acceso no fue recuperado correctamente.";
+    }
+}
+
+?>
---
+++ MNProyecto-main/MNProyecto-main/Models/homeModel.php
@@ -0,0 +1,61 @@
+<?php
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/Curso/Models/connect.php';
+
+    function ValidarInicioSesionModel($correo, $contrasenna)
+    {
+        try
+        {
+            $context = OpenDB();
+
+            $sp = "CALL ValidarInicioSesion('$correo', '$contrasenna')";
+            $respuesta = $context -> query($sp);
+
+            CloseDB($context);            
+            return $respuesta;
+        }
+        catch(Exception $error)
+        {
+            RegistrarError($error);
+            return null;
+        }
+    }
+
+    function RegistrarUsuarioModel($nombre, $correo, $identificacion, $contrasenna)
+    {
+        try
+        {
+            $context = OpenDB();
+
+            $sp = "CALL RegistrarUsuario('$nombre', '$correo', '$identificacion', '$contrasenna')";
+            $respuesta = $context -> query($sp);
+
+            CloseDB($context);            
+            return $respuesta;
+        }
+        catch(Exception $error)
+        {
+            RegistrarError($error);
+            return false;
+        }
+    }
+
+    function ValidarCorreoModel($correo)
+    {
+        try
+        {
+            $context = OpenDB();
+
+            $sp = "CALL ValidarCorreo('$correo')";
+            $respuesta = $context -> query($sp);
+
+            CloseDB($context);            
+            return $respuesta;
+        }
+        catch(Exception $error)
+        {
+            RegistrarError($error);
+            return null;
+        }
+    }
+
+?>
